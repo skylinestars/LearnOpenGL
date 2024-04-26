e4f8bcb6baf638e81dd1390a9b372004e94f6d5c
luke <1140643368@qq.com>
2024-04-26 17:57:14 +0800

    2024-4-26-16

--- bin/GLSL/5.1.framebuffers_screen.fs
+++ bin/GLSL/5.1.framebuffers_screen.fs
@@ -35,5 +35,6 @@ void main()
     for(int i = 0; i < 9; i++)
         col += sampleTex[i] * kernel[i];
 
-    FragColor = vec4(col, 1.0);
+    //FragColor = vec4(col, 1.0);
+	FragColor = texture(screenTexture, TexCoords);
 }
